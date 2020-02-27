c0972573b7c15beb89ea3336cc8139a795458b9a
Jorge Yau <codenameyau@gmail.com>
2020-02-27 11:53:24 -0500

    Fix storybook

--- .storybook/Welcome.stories.js
+++ .storybook/Welcome.stories.js
@@ -1,55 +1,10 @@
-import { h } from 'preact';
+import React from 'react';
 import styled from 'styled-components';
 import { linkTo } from '@storybook/addon-links';
 
-const Main = styled.div`
-  padding: 1em;
-`;
-
-Main.displayName = 'Main';
-
-const Title = ({ children, ...props }) => <h1 {...props}>{children}</h1>;
-
-const Link = ({ children, href, ...props }) => (
-  <a
-    href={href}
-    {...props}
-    style={{
-      color: '#1474f3',
-      textDecoration: 'none',
-      borderBottom: '1px solid #1474f3',
-      paddingBottom: 2,
-    }}
-  >
-    {children}
-  </a>
-);
-
-const NavButton = ({ children, ...props }) => (
-  <button
-    {...props}
-    type="button"
-    style={{
-      color: '#1474f3',
-      textDecoration: 'none',
-      borderBottom: '1px solid #1474f3',
-      paddingBottom: 2,
-      borderTop: 'none',
-      borderRight: 'none',
-      borderLeft: 'none',
-      backgroundColor: 'transparent',
-      padding: 0,
-      cursor: 'pointer',
-      font: 'inherit',
-    }}
-  >
-    {children}
-  </button>
-);
-
 const Welcome = () => (
-  <Main>
-    <Title>Welcome to storybook</Title>
+  <div>
+    <h1>Welcome to storybook</h1>
     <p>This is a UI component dev environment for your app.</p>
     <p>
       A story is a single state of one or more UI components. You can have as many stories
@@ -64,16 +19,16 @@ const Welcome = () => (
       Usually we create stories with smaller UI components in the app.
       <br />
       Have a look at the{' '}
-      <Link
+      <a
         href="https://storybook.js.org/basics/writing-stories"
         target="_blank"
         rel="noopener noreferrer"
       >
         Writing Stories
-      </Link>{' '}
+      </a>{' '}
       section in our documentation.
     </p>
-  </Main>
+  </div>
 );
 
 export default {
