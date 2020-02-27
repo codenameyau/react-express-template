0402f2d9a158587d5a8f0eeaad304fcaaa51ae78
Jorge Yau <codenameyau@gmail.com>
2020-02-27 11:37:15 -0500

    Kill css

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,47 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
 import logo from './logo.svg';
-import './App.css';
 import request from 'app/utils/request';
+
+const StyledApp = styled.div`
+  text-align: center;
+
+  .App-logo {
+    height: 40vmin;
+    pointer-events: none;
+  }
+
+  @media (prefers-reduced-motion: no-preference) {
+    .App-logo {
+      animation: App-logo-spin infinite 20s linear;
+    }
+  }
+
+  .App-header {
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+  }
+
+  .App-link {
+    color: #61dafb;
+  }
+
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
 
 function App() {
   const [ready, setReady] = useState(false);
@@ -17,7 +57,7 @@ function App() {
   }, [ready]);
 
   return ready ? (
-    <div className="App">
+    <StyledApp>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>{apiData && JSON.stringify(apiData, null, 2)}</p>
@@ -30,7 +70,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </StyledApp>
   ) : null;
 }
 
