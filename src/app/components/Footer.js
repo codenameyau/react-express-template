38cb98bffd3fb31aa321834c523c0acf2edbd424
Jorge Yau <codenameyau@gmail.com>
2020-03-11 20:28:43 -0400

    Add source code link in footer

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,7 +1,23 @@
+import React from 'react';
 import styled from 'styled-components';
 
-export const Footer = styled.div`
+export const FooterContainer = styled.div`
   width: 100%;
-  height: 5em;
+  padding: 1.5em 1em;
   background: #393b40;
+  text-align: center;
+
+  a {
+    color: #eee;
+  }
 `;
+
+export const Footer = props => {
+  return (
+    <FooterContainer>
+      <a href="https://github.com/codenameyau/react-express-template">
+        https://github.com/codenameyau/react-express-template
+      </a>
+    </FooterContainer>
+  );
+};
